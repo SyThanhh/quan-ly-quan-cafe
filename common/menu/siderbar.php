5caf12f062037cf892d80f3972546c8274c5598f
SyThanhh <nguyensythanh27102003@gmail.com>
2024-10-23 00:17:20 +0700

    update first

--- common/menu/siderbar.php
+++ common/menu/siderbar.php
@@ -15,7 +15,7 @@
 <li class="nav-item active">
     <a class="nav-link" href="index.html">
         <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Trang chủ (Thống kê doanh thu)</span></a>
+        <span>Trang chủ</span></a>
 </li>
 
 <!-- Divider -->
@@ -155,7 +155,23 @@
         </div>
     </div>
 </li>
-
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#manageStatistical"
+        aria-expanded="true" aria-controls="manageStatistical">
+        <i class="fas fa-fw fa-wrench"></i>
+        <span>Thống kê doanh thu</span>
+    </a>
+    <div id="manageStatistical" class="collapse" aria-labelledby="headingUtilities"
+        data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Utilities:</h6>
+            <a class="collapse-item" href="utilities-color.html">Colors</a>
+            <a class="collapse-item" href="utilities-border.html">Borders</a>
+            <a class="collapse-item" href="utilities-animation.html">Animations</a>
+            <a class="collapse-item" href="utilities-other.html">Other</a>
+        </div>
+    </div>
+</li>
 
 <!-- Divider -->
 <hr class="sidebar-divider">
