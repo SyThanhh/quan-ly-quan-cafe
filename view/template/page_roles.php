9654049af25e304caad143224e631322ac49d8be
Niwangguu2323 <hoanghanhnhibmt@gmail.com>
2024-12-04 02:17:48 +0700

    Nhi-capnhatlaivitri-xong

--- view/template/page_roles.php
+++ view/template/page_roles.php
@@ -223,6 +223,40 @@
                 </nav>
 
                 <!--  Nội dung trang  -->
+                <?php
+                // Kiểm tra nếu có tìm kiếm
+                $searchKeyword = isset($_POST['name-search']) ? $_POST['name-search'] : '';
+
+                // Số bản ghi trên mỗi trang
+                $recordsPerPage = 5;
+
+                // Tính tổng số bản ghi
+                $totalRecordsQuery = "SELECT COUNT(*) as total FROM employee WHERE Roles > 1";
+                if ($searchKeyword !== '') {
+                    $totalRecordsQuery .= " AND (FirstName LIKE '%$searchKeyword%' OR LastName LIKE '%$searchKeyword%')";
+                }
+                $totalRecordsResult = $database->select($totalRecordsQuery);
+                $totalRecords = $totalRecordsResult->fetch_assoc()['total'];
+
+                // Tính tổng số trang
+                $totalPages = ceil($totalRecords / $recordsPerPage);
+
+                // Lấy trang hiện tại từ URL, mặc định là trang 1
+                $page = isset($_GET['page_number']) ? (int)$_GET['page_number'] : 1;
+                $page = ($page > $totalPages) ? $totalPages : $page;
+                $page = ($page < 1) ? 1 : $page;
+
+                // Tính offset để lấy dữ liệu
+                $offset = ($page - 1) * $recordsPerPage;
+
+                // Truy vấn danh sách nhân viên với phân trang và tìm kiếm
+                $query = "SELECT * FROM employee WHERE Roles > 1";
+                if ($searchKeyword !== '') {
+                    $query .= " AND (FirstName LIKE '%$searchKeyword%' OR LastName LIKE '%$searchKeyword%')";
+                }
+                $query .= " LIMIT $offset, $recordsPerPage";
+                $employees = $database->select($query);
+                ?>
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12">
@@ -234,7 +268,7 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="input-group">
-                                            <input type="text" class="form-control" id="name-search" placeholder="Tìm nhân viên theo tên">
+                                            <input type="text" class="form-control" id="name-search" placeholder="Tìm nhân viên theo tên" value="<?php echo htmlspecialchars($searchKeyword); ?>">
                                             <div class="input-group-append">
                                                 <button class="btn btn-outline-secondary search-button m-0" type="button">
                                                     <i class="fas fa-search"></i>
@@ -265,9 +299,6 @@
                                     </thead>
                                     <tbody>
                                         <?php
-                                            // Truy vấn danh sách nhân viên
-                                            $employees = $database->select("SELECT * FROM employee WHERE Roles > 1");
-
                                             // Hiển thị danh sách nhân viên
                                             if ($employees) {
                                                 while ($row = $employees->fetch_assoc()) { // Sử dụng fetch_assoc() từ mysqli
@@ -301,11 +332,42 @@
                                         ?>
                                     </tbody>
                                 </table>
+
+                                <!-- Phân trang -->
+                                <div class="row justify-content-end mr-1">
+                                    <nav aria-label="Page navigation example">
+                                        <ul class="pagination">
+                                            <!-- Nút Previous -->
+                                            <li class="page-item <?php if ($page <= 1) echo 'disabled'; ?>">
+                                                <a class="page-link" href="index.php?page=page_roles&page_number=<?php echo ($page > 1) ? $page - 1 : 1; ?>&search=<?php echo urlencode($searchKeyword); ?>" aria-label="Previous">
+                                                    <span aria-hidden="true">&laquo;</span>
+                                                </a>
+                                            </li>
+
+                                            <!-- Các trang số -->
+                                            <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                                                <li class="page-item <?php if ($i == $page) echo 'active'; ?>">
+                                                    <a class="page-link" href="index.php?page=page_roles&page_number=<?php echo $i; ?>&search=<?php echo urlencode($searchKeyword); ?>">
+                                                        <?php echo $i; ?>
+                                                    </a>
+                                                </li>
+                                            <?php endfor; ?>
+
+                                            <!-- Nút Next -->
+                                            <li class="page-item <?php if ($page >= $totalPages) echo 'disabled'; ?>">
+                                                <a class="page-link" href="index.php?page=page_roles&page_number=<?php echo ($page < $totalPages) ? $page + 1 : $totalPages; ?>&search=<?php echo urlencode($searchKeyword); ?>" aria-label="Next">
+                                                    <span aria-hidden="true">&raquo;</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             <!-- Cuối trang -->
             <?php include_once('./common/footer/footer.php'); ?>
         </div>    
