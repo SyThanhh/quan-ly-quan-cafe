c895d4826e73ee8587bb839075f5c3415cdde102
Nguyenbadien <diennguyenba777@gmail.com>
2024-12-08 20:31:52 +0700

    dien update

--- controller/cRequest.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once('./model/mRequest.php');
-    class cRequest {
-        public function cDeleteRq($RequestID) {
-            $p = new mRequest();
-            return $p->mDeleteRq($RequestID);
-        }
-    }
-?>
